fde11f6f89723f78cacde319430de8277ae1fbc7
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2022-11-28 17:46:51 +0300

    Add tests and function placeholders

---
+++ services/lichess.py
@@ -0,0 +1,19 @@
+def get_username_of(players, side):
+    """Ищет среди списка `players` и возвращает имя игрока  играющего
+    за сторону `side`"""
+    pass
+
+
+def is_lose(game, username):
+    """Return true if player with `username` losed the game``"""
+    pass
+
+
+def is_side(username, side_name):
+    """Return true if player played with si """
+    pass
+
+
+def is_black():
+    """"""
+    pass
---
+++ tests/test_lichess.py
@@ -0,0 +1,23 @@
+from services.lichess import (
+    get_username_of,
+    is_lose,
+    is_side,
+)
+
+
+def test_get_username_of():
+   pass 
+
+
+def test_is_lose():
+    pass
+
+
+def test_is_white():
+    pass
+
+
+def test_is_black()
+    pass
+
+
