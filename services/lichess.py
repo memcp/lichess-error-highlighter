872420a3cc660a916072b7df25347d812a1ce8a8
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2022-11-28 17:47:55 +0300

    Translate comments into russian

--- services/lichess.py
+++ services/lichess.py
@@ -1,19 +1,14 @@
 def get_username_of(players, side):
-    """Ищет среди списка `players` и возвращает имя игрока  играющего
-    за сторону `side`"""
+    """Ищет среди списка `players` и возвращает имя игрока играющего за сторону `side`"""
     pass
 
 
 def is_lose(game, username):
-    """Return true if player with `username` losed the game``"""
+    """Определяет проиграл ли игрок с `username` игра `game`"""
     pass
 
 
 def is_side(username, side_name):
-    """Return true if player played with si """
+    """Определяет играл ли игрок с `username` за сторону `side_name`"""
     pass
 
-
-def is_black():
-    """"""
-    pass
