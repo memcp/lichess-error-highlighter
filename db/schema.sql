0854e32e9b66dc8cd3f6f675df821e0f75c08b32
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2022-11-29 16:46:25 +0300

    Create tables for current player, games and openings

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS opening;
+DROP TABLE IF EXISTS game;
+
+CREATE TABLE player (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE opening(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  short_name TEXT,
+
+  UNIQUE(name, short_name)
+);
+
+CREATE TABLE game (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  lichess_id TEXT UNIQUE NOT NULL,
+  winner TEXT,
+  player_id INTEGER NOT NULL,
+  opening_id INTEGER NOT NULL,
+
+  FOREIGN KEY (player_id) REFERENCES player (id),
+  FOREIGN KEY (opening_id) REFERENCES opening (id)
+);
