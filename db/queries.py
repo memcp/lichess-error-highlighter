a449c521743e245ab177f3d209e204463464e622
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2022-11-30 19:40:06 +0300

    Add route to get winrates of openings

---
+++ db/queries.py
@@ -0,0 +1,63 @@
+def insert_player(conn, username):
+    """Добавляет игрока в таблицу, возвращает id"""
+    sql = "INSERT OR IGNORE INTO player (username) VALUES (?);"
+    params = (username,)
+    curr = conn.execute(sql, params)
+    player_id = curr.lastrowid
+    curr.close()
+    conn.commit()
+    return player_id
+
+
+def insert_opening(conn, name, short_name):
+    """Добавляет дебют в таблицу, возвращает id"""
+    sql = "INSERT OR IGNORE INTO opening (name, short_name) VALUES (?, ?);"
+    params = (name, short_name)
+    curr = conn.execute(sql, params)
+    opening_id = curr.lastrowid
+    curr.close()
+    conn.commit()
+    return opening_id
+
+
+def insert_game(conn, lichess_id, is_lose, player_id, opening_id):
+    """Добавляет игру в таблицу"""
+    sql = """INSERT OR IGNORE INTO game(lichess_id, is_lose, player_id, opening_id)
+             VALUES (?, ?, ?, ?);"""
+    params = (lichess_id, is_lose, player_id, opening_id)
+    conn.execute(sql, params)
+    conn.commit()
+
+
+def get_number_of_games(conn, opening_short_name=None):
+    """Получить общее количество сыграных игр или в конкретном дебюте"""
+    sql = """SELECT 
+               count(game.id) as number_of_games
+             FROM 
+               game INNER JOIN opening ON game.opening_id = opening.id AND 
+                                          opening.short_name = ?
+          """
+    params = (opening_short_name,)
+    games = conn.execute(sql, params)
+    return games.fetchone()['number_of_games']
+
+
+def win_games_grouped_by_opening(conn):
+    """Получить статистику о выигранных играх для игрока из таблицы 'player' 
+       сгруппированную по дебютам"""
+    sql = """SELECT 
+               opening.short_name,
+               count(opening.short_name) as wins_counter
+             FROM 
+               game INNER JOIN player ON game.player_id = player.id AND
+                                         game.is_lose = false
+                    LEFT JOIN opening ON game.opening_id = opening.id
+             GROUP BY
+               opening.short_name;
+           """
+    curr = conn.cursor()
+    curr.execute(sql)
+    games = curr.fetchall()
+    curr.close()
+    return games
+
---
+++ leh.py
@@ -0,0 +1,82 @@
+import berserk
+from flask import Flask, render_template, stream_template, jsonify, url_for, Response
+    
+from services.lichess import (
+    get_username_of, is_lose, played_for, group_lost_games_by_opening,
+    get_short_opening_name
+)
+from db import *
+from db.queries import (
+    insert_player, insert_opening, insert_game, win_games_grouped_by_opening,
+    get_number_of_games
+)
+
+
+def create_app():
+    import db
+
+    app = Flask(__name__)
+    app.config['DATABASE'] = 'db/leh.db'
+    db.init_app(app)
+
+    return app
+
+
+app = create_app()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/opening-stats')
+def games():
+    conn = get_db()
+    openening_stats = {}
+    win_opening_groups = win_games_grouped_by_opening(conn)
+    
+    for group in win_opening_groups:
+        number_of_wins = group['wins_counter']
+        number_of_games = get_number_of_games(conn, group['short_name'])
+        
+        try:
+            winrate = str((number_of_wins / number_of_games) * 100) + '%'
+        except:
+            winrate = "-"
+
+        openening_stats[str(group['short_name'])] = {
+            'wins': number_of_wins,
+            'games': number_of_games,
+            'winrate': winrate
+        }
+
+    return openening_stats
+
+
+@app.route('/upload-games/<token>')
+def load_games(token):
+    session = berserk.TokenSession(token)
+    client = berserk.Client(session)
+    account = client.account.get()
+    username = account.get('username')
+
+    conn = get_db()
+    games = client.games.export_by_player(username, opening=True, rated=True)
+    player_id = insert_player(conn, username)
+
+    for game in games:
+        game_id = game.get('id')
+        game_is_lost = is_lose(game, username)
+
+        if 'opening' not in game:
+            print(game_id)
+            continue
+        
+        opening_name = game.get('opening').get('name')
+        opening_short_name = get_short_opening_name(opening_name)
+        opening_id = insert_opening(conn, opening_name, opening_short_name)
+
+        insert_game(conn, game_id, game_is_lost, player_id, opening_id)
+
+    return ""
