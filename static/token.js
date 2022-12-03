1d434cca6f553c6469eeb69c7cfbbdf4f4606355
Ilyas Garipov <main.ilyasgaripov@gmail.com>
2022-12-03 20:29:25 +0300

    Redirect on client

--- static/token.js
+++ static/token.js
@@ -2,6 +2,7 @@ async function setAccessToken() {
     const accessTokenForm = document.querySelector('.access-token-form');
     const accessToken = accessTokenForm.elements['access_token'].value;
     await fetch(`http://127.0.0.1:5000/create-games/${accessToken}`)
+    window.location.href = "http://127.0.0.1:5000/opening-stats";
 }
 
 const submitButton = document.querySelector('.set-access-token');
